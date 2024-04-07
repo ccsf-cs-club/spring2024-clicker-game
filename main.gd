a9d115e73763f84014ea884140d217e3ad66c350
Vena <yppahhappy.com@gmail.com>
2024-04-06 19:14:52 -0700

    Dynamic button click counter created

--- main.gd
+++ main.gd
@@ -3,12 +3,17 @@ extends Node2D
 # LabelCounter can now be selected in the inspector
 @export_node_path("Label") var LabelCounter
 
+# Button counter is linked to
+@export_node_path("Button") var CounterButton
+
+var counter = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Text node
 	print("Label to display: ", LabelCounter)
 	var label = get_node(LabelCounter)
-	label.text = "Hello from code!~"
+	label.text = "How high can you click!"
 
 	# Font for Size
 	var font = FontFile.new()
@@ -19,6 +24,21 @@ func _ready():
 	# https://docs.godotengine.org/en/stable/classes/class_label.html#theme-property-descriptions
 	label.add_theme_font_override("font",load("res://FontRoboto/Roboto-Black.ttf"))
 	label.add_theme_font_size_override("font_size", 52)
+	
+	# Button Counter node
+	var counterButton = get_node(CounterButton)
+	if counterButton:
+		counterButton.connect("pressed", Callable(self, "increment_counter"))
+	else:
+		print("CounterButton not found.")
+	
+
+func increment_counter():
+	counter += 1
+	var label = get_node(LabelCounter)
+	label.text = str(counter)
+	print("Counter is now: ", counter)
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
